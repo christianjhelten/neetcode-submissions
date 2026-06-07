d2737cd83bbf376395e48bfbfe19ba95bf667f98
christianjhelten <christian.j.helten@gmx.de>
2026-06-06 18:28:55 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        win = []
+        win.append(0)
+        for i in range(len(prices)-1):
+            if prices[i] < max(prices[i+1:]):
+                win.append(max(prices[i+1:]) - prices[i])
+            else:
+                continue
+        
+        return max(win)
